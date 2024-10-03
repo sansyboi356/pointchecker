404342eb85c4ac0e5b18042e3935613387706c17
sansyboi356 <sansyboi356@gmail.com>
2024-10-03 18:31:07 -0400

    Update wofur point checker.py

--- wofur point checker.py
+++ wofur point checker.py
@@ -7,3 +7,9 @@ import autoit as AI
 pag.hotkey("ctrl" , "win" , "right")
 AO.open("brave")
 time.sleep(1)
+# define
+def leftclick (X , Y, lc = 1, spd = -1):
+    AI.mouse_click(x = X, y = Y, clicks = lc, speed = spd)
+## back to the scheduled program
+leftclick(282, 83)
+pag.typewrite("https://www.roblox.com/games/2727067538/2X-World-Zero-Anime-RPG")
