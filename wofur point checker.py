33293ee9893089997acf7407a7107a150bdb6e6f
sansyboi356 <sansyboi356@gmail.com>
2024-10-03 17:53:59 -0400

    Update wofur point checker.py

--- wofur point checker.py
+++ wofur point checker.py
@@ -1,1 +1,7 @@
-import 
+## Cocain imports
+import time
+import AppOpener as AO 
+import pyautogui as pag
+##begin the fuckery (for a second time)
+pag.hotkey("ctrl" , "win" , "right")
+AO.open("brave")
