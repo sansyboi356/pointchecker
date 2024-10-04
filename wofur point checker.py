c8899e58bd78ded40f599976b80eeedb7cdc4be3
sansyboi356 <sansyboi356@gmail.com>
2024-10-04 17:24:19 -0400

    basically adding the entire screenshot stuff

--- wofur point checker.py
+++ wofur point checker.py
@@ -7,13 +7,29 @@ import ait
 import cv2 as ocv
 import pytesseract as pyt
 import os
+import PIL
+import pygetwindow as pgw
+
 ## begin the fuckery (for a second time)
 pag.hotkey("ctrl" , "win" , "right")
 AO.open("brave")
 time.sleep(1)
 ## defines
+
+## so I dont have to write it over and over
 def leftclick (X , Y, lc = 1, spd = -1):
     AI.mouse_click(x = X, y = Y, clicks = lc, speed = spd)
+
+## for image writing bs
+ssfolder = "C:/projects/pointchecker/Images"
+path = os.path.join(ssfolder, "screenshot.png")
+region = (1123, 212, 1686, 648)
+screenshot = pag.screenshot(region = region )
+window_name = "Roblox"
+window = pgw.getWindowsWithTitle(window_name)
+
+
+
 ## back to the scheduled program
 leftclick(282, 83)
 ## game website
@@ -27,5 +43,11 @@ time.sleep(20)
 leftclick(962, 920)
 time.sleep(35)
 ##ok we have roblox open, now we go to open the guild menu
-AI.send("l")
+AI.send("L")
+time.sleep(10)
 ##open cv time YIPPE
+if window:
+    window = window[0]
+    window.activate()
+screenshot.save(path)
+
